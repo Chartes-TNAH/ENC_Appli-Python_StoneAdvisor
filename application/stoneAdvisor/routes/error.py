13fcb61063d3843351c2cfabf69ac830f3252bd3
Rebecca <webecca@tnah.com>
2020-04-01 10:15:41 +0200

    Pages d'erreurs

---
+++ application/stoneAdvisor/routes/error.py
@@ -0,0 +1,11 @@
+from flask import render_template
+from ..app import app
+
+# Erreurs.
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('error/404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('error/500.html'), 500
